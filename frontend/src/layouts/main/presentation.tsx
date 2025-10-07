623b2ecdd6ebf7ff7040e5aa0d4452295c7cd562
federico tropiane <fedetrop23@gmail.com>
2025-10-07 00:22:25 -0300

    generamos estilos mobile

--- frontend/src/layouts/main/presentation.tsx
+++ frontend/src/layouts/main/presentation.tsx
@@ -2,11 +2,9 @@ import { config } from "../../config"
 import Button from "../../components/inputs/button"
 function Presentation() {
     return <div className="presentation">
-        <div className="presentation-header">
             <h2 className="secondTitleFont">{config.presentationData.title}</h2>
             <p className="textFont">{config.presentationData.description}</p>
             <Button link={config.presentationData.link} text={config.presentationData.linkText} className="button buttonFont" />
-        </div>
         
 
     </div>
