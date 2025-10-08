266754aca99663669a453256af85d864835b7692
federico tropiane <fedetrop23@gmail.com>
2025-10-07 23:26:23 -0300

    generamos estilo mobile

--- frontend/src/layouts/main/main.tsx
+++ frontend/src/layouts/main/main.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./home";
-import AboutUs from "../../components/pages/aboutUs";
 import { TicketsManager } from "../../components/pages/TicketsManager";
 import { Login } from "../../components/pages/Login";
 function Main() {
@@ -9,7 +8,6 @@ function Main() {
     <main>
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/quien-soy" element={<AboutUs />} />
             <Route path="/tickets-manager" element={<TicketsManager/>}/>
             <Route path="/login" element={<Login/>}/>
         </Routes>
--- frontend/src/layouts/main/presentation.tsx
+++ frontend/src/layouts/main/presentation.tsx
@@ -1,10 +1,9 @@
 import { config } from "../../config"
-import Button from "../../components/inputs/button"
 function Presentation() {
     return <div className="presentation">
             <h2 className="secondTitleFont">{config.presentationData.title}</h2>
             <p className="textFont">{config.presentationData.description}</p>
-            <Button link={config.presentationData.link} text={config.presentationData.linkText} className="button buttonFont" />
+            <a href="#planes" className="button buttonFont">Ver planes</a>
         
 
     </div>
