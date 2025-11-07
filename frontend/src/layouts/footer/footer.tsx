14a14e055326eb259dd05ecbf62c4d5b0eb1b271
federico tropiane <fedetrop23@gmail.com>
2025-11-06 21:05:26 -0300

    Tratamos de solucionar falla de imports

--- frontend/src/layouts/footer/footer.tsx
+++ frontend/src/layouts/footer/footer.tsx
@@ -1,6 +1,6 @@
 import { config } from "../../config.ts";
-import Form from "./Form";
-import SocialMedia from "./SocialMedia";
+import Form from "./Form.tsx";
+import SocialMedia from "./SocialMedia.tsx";
 
 
 function Footer() {
